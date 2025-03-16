692f8dc49b5233ab88b23e4346ef3ffc153fd3ba
haruyuki@hailfall.macbook <haruyuki@relicsnow.info>
2025-03-16 15:46:46 +0900

    JSDocで型を追加

--- main.mjs
+++ main.mjs
@@ -1,15 +1,16 @@
+/** @type {number} */
 let count = 0;
 
 function onClickPlus() {
   count += 1;
-  const countElem = document.querySelector("span.count");
-  countElem.textContent = count;
+  const countElem = /** @type {HTMLSpanElement} */ (document.querySelector("span.count"));
+  countElem.textContent = count.toString();
 }
 
 function onClickMinus() {
   count -= 1;
-  const countElem = document.querySelector("span.count");
-  countElem.textContent = count;
+  const countElem = /** @type {HTMLSpanElement} */ (document.querySelector("span.count"));
+  countElem.textContent = count.toString();
 }
 
 document.querySelector("button.plus").addEventListener("click", onClickPlus);
